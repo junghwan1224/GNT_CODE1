6ff15d0278a36c29c0f0e8d860b83b46acc669ea
junghwan1224 <junghwan1224@gmail.com>
2023-04-09 14:28:09 +0900

    10815 숫자 카드

---
+++ src/ps10815/Main.java
@@ -0,0 +1,35 @@
+package ps10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/10815
+ * */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        Set<Integer> set = new HashSet<>();
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            set.add(arr[i]);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(n) ? "1" : "0").append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
